b0332ae7dfb9c301734e23c8cdddc15de0d1625b
Ward Quinlan <wardquinlan@outlook.com>
2022-12-19 13:21:02 -0500

    continuation

---
+++ resources/createdb.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [ `whoami` != 'postgres' ]; then
+  echo $0: must run as \'postgres\'
+  exit 1
+fi
+
+if [ "$ECON_DATABASE" = $"" ]; then
+  echo $0: ECON_DATABASE not defined
+  exit 1
+fi
+
+if [ "$ECON_USER" = $"" ]; then
+  echo $0: ECON_USER not defined
+  exit 1
+fi
+
+if [ "$ECON_PASSWORD" = $"" ]; then
+  echo $0: ECON_PASSWORD not defined
+  exit 1
+fi
+
+echo $0: attempting to drop database \'$ECON_DATABASE\'
+dropdb $ECON_DATABASE 2>/dev/null
+if [ $? != 1 ]; then
+  echo $0: database \'$ECON_DATABASE\' not found';' skipping drop
+fi
+
+echo $0: attempting to create database \'$ECON_DATABASE\'
+createdb $ECON_DATABASE
+if [ $? != 0 ]; then
+  echo $0: could not create database \'$ECON_DATABASE\'
+  exit 1
+fi
+
+echo $0: attempting to create user \'$ECON_USER\'
+psql << EOF
+  DROP USER IF EXISTS $ECON_USER;
+  CREATE USER $ECON_USER WITH PASSWORD '$ECON_PASSWORD';
+EOF
+
+echo $0: database $ECON_DATABASE created
+
--- src/econ/Main.java
+++ src/econ/Main.java
@@ -1,5 +1,14 @@
 package econ;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -7,6 +16,22 @@ public class Main {
 	private static Log log = LogFactory.getFactory().getInstance(Main.class);
 	public static void main(String[] args) {
 		log.info("Application starting");
-		System.out.println("Hello World!");
+		JFrame frame = new JFrame("Econ");
+		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new JPanel() {
+        	@Override
+        	public void paintComponent(Graphics g) {
+        		super.paintComponent(g);
+        		setBackground(new Color(0xc0c0c0));
+        	    int widthPanel = getWidth();
+        	    FontMetrics m = g.getFontMetrics(g.getFont());
+        	    int heightFont = m.getHeight();
+        	    int widthText = m.stringWidth("Chart Title");
+        	    g.drawString("Chart Title", (widthPanel - widthText) / 2, heightFont);
+        	    g.drawLine(0, 0, 100, 100);
+        	}
+        });
+        frame.setVisible(true);
 	}
 }
