e2034c882b0840449a002407715caa2259cd2009
Ward Quinlan <wardquinlan@outlook.com>
2022-12-19 10:42:58 -0500

    Initial commit

---
+++ src/econ/Main.java
@@ -0,0 +1,12 @@
+package econ;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Main {
+	private static Log log = LogFactory.getFactory().getInstance(Main.class);
+	public static void main(String[] args) {
+		log.info("Application starting");
+		System.out.println("Hello World!");
+	}
+}
