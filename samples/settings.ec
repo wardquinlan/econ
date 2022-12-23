5b03ee1048e97e2208734e1dcda03b891828535c
Ward Quinlan <wardquinlan@outlook.com>
2022-12-23 09:22:06 -0500

    refactoring

--- samples/settings.ec
+++ samples/settings.ec
@@ -5,3 +5,6 @@ settings.padding.top    = 5;
 settings.padding.left   = 5;
 settings.padding.bottom = 5;
 settings.padding.right  = 5;
+
+-- used to mark that settings have completed loading
+settings.loaded = 1;
--- src/econ/EconContext.java
+++ src/econ/EconContext.java
@@ -1,10 +1,13 @@
 package econ;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EconContext {
   private String script;
   private ArrayList<Chart> charts = new ArrayList<>();
+  private Map<String, Symbol> symbolTable = new HashMap<>();
 
   public String getScript() {
     return script;
@@ -16,5 +19,9 @@ public class EconContext {
   
   public ArrayList<Chart> getCharts() {
     return charts;
+  }
+
+  public Map<String, Symbol> getSymbolTable() {
+    return symbolTable;
   }
 }
