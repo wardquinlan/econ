5cfbc352b5b1c1c17781077fbe22159ef041d487
Ward Quinlan <wardquinlan@outlook.com>
2022-12-24 18:36:37 -0500

    Clean up colors

--- samples/settings.ec
+++ samples/settings.ec
@@ -1,7 +1,9 @@
 include "colors.ec";
 
 settings.panel.background.color = WHITE;
-settings.chart.background.color = YELLOW;
+settings.chart.background.color = LIGHT_GRAY;
+settings.chart.line.color = GRAY;
+settings.chart.font.color = DARK_GRAY;
 
 -- used to mark that settings have completed loading
 settings.loaded = 1;
--- src/econ/ChartsPanel.java
+++ src/econ/ChartsPanel.java
@@ -1,6 +1,5 @@
 package econ;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
@@ -11,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ChartsPanel extends JPanel {
+  private static final long serialVersionUID = 8263376302676172047L;
   private Context ctx;
   private Panel panel;
   private JButton button1 = new JButton("<<");
@@ -37,7 +37,12 @@ public class ChartsPanel extends JPanel {
   @Override
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
-    setBackground(new Color((int) ctx.get("settings.panel.background.color")));
+    final Color PANEL_BACKGROUND = new Color((int) ctx.get("settings.panel.background.color"));
+    final Color CHART_BACKGROUND = new Color((int) ctx.get("settings.chart.background.color"));
+    final Color CHART_RECT = new Color((int) ctx.get("settings.chart.line.color"));
+    final Color CHART_FONT = new Color((int) ctx.get("settings.chart.font.color"));
+    
+    setBackground(PANEL_BACKGROUND);
     FontMetrics m = g.getFontMetrics(g.getFont());
     
     // button1.setBounds(getWidth() - 100 - (int) ctx.get("settings.panel.padding.right"), getHeight() - 20 - (int) ctx.get("settings.panel.padding.bottom"), 50, 20);
@@ -47,11 +52,15 @@ public class ChartsPanel extends JPanel {
     for (int i = 0; i < panel.getCharts().size(); i++) {
       Chart chart = panel.getCharts().get(i);
       int y = m.getHeight() + i * chartHeight;
-      g.drawString(chart.getLabel(), 0, y);
-      g.drawRect(0, y, getWidth() - 1, chartHeight - m.getHeight() - 1);
-      g.setColor(new Color((int) ctx.get("settings.chart.background.color")));
+      
+      g.setColor(CHART_BACKGROUND);
       g.fillRect(0, y, getWidth() - 1, chartHeight - m.getHeight() - 1);
       
+      g.setColor(CHART_RECT);
+      g.drawRect(0, y, getWidth() - 1, chartHeight - m.getHeight() - 1);
+
+      g.setColor(CHART_FONT);
+      g.drawString(chart.getLabel(), 0, y);
     }
     /*
     g.drawLine(0, 0, 100, 100);
