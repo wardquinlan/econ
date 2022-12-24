65256263adfdc62ae27bae0cb06b22e7cc99363b
Ward Quinlan <wardquinlan@outlook.com>
2022-12-23 20:47:41 -0500

    research w/ setBounds

--- src/econ/EconPanel.java
+++ src/econ/EconPanel.java
@@ -1,17 +1,33 @@
 package econ;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class EconPanel extends JPanel {
   private EconContext context;
+  private JButton button = new JButton("Hi");
   
   public EconPanel(EconContext context) {
     super();
     this.context = context;
+    JPanel gridPanel = new JPanel();
+    gridPanel.setPreferredSize(new Dimension(200, 200));
+    gridPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+    setLayout(new BorderLayout());
+    setLayout(null);
+    //add(gridPanel, BorderLayout.WEST);
+    //JPanel panel = new JPanel();
+    //panel.setBorder(BorderFactory.createLineBorder(Color.RED));
+    //add(panel, BorderLayout.CENTER);
+    //button.setBounds(10, 10, getWidth() / 2, getHeight() / 2);
+    //add(button);
   }
 
   @Override
@@ -24,11 +40,18 @@ public class EconPanel extends JPanel {
     int widthText = m.stringWidth("Chart Title");
     g.drawString("Chart Title", (widthPanel - widthText) / 2, heightFont);
     
+    if (button == null) {
+      button = new JButton("Hi");
+    }
+    button.setBounds(10, 10, getWidth() / 2, getHeight() / 2);
+    add(button);
+    
     int x = (int) context.get("settings.panel.padding.left");
     int y = m.getHeight();
     int width = getWidth() - x - (int) context.get("settings.panel.padding.right");
     int height = getHeight() - y - (int) context.get("settings.panel.padding.bottom");
     g.drawRect(x, y, width, height);
+    
     /*
     g.drawLine(0, 0, 100, 100);
     g.drawRect(10, 10, 500, 200);
