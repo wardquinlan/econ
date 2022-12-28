edd19642d80d0e5401bae1f12a66244d92f38cf3
Ward Quinlan <wardquinlan@outlook.com>
2022-12-27 20:04:14 -0500

    Clean up panel paint code

--- src/econ/ChartsPanel.java
+++ src/econ/ChartsPanel.java
@@ -72,25 +72,18 @@ public class ChartsPanel extends JPanel {
     // button1.setBounds(getWidth() - 100 - (int) ctx.get("settings.panel.padding.right"), getHeight() - 20 - (int) ctx.get("settings.panel.padding.bottom"), 50, 20);
     // button2.setBounds(getWidth() - 50 - (int) ctx.get("settings.panel.padding.right"), getHeight() - 20 - (int) ctx.get("settings.panel.padding.bottom"), 50, 20);
     
-    //int chartHeight = (getHeight() - CHART_SEPARATOR) / panel.getCharts().size();
+    Stroke strokeOrig = ((Graphics2D) g).getStroke();
+    Stroke strokeGridlines = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {1}, 0);
     int chartWidth = getWidth() - 1 - 2 * CHART_HPADDING;
     int y = CHART_SEPARATOR;
     for (int i = 0; i < panel.getCharts().size(); i++) {
       Chart chart = panel.getCharts().get(i);
       int chartHeight = (getHeight() - CHART_SEPARATOR) * chart.getSpan() / 100;
-      
+
+      ((Graphics2D) g).setStroke(strokeOrig);
       // Fill the rectangle with the background color
       g.setColor(CHART_BACKGROUND);
       g.fillRect(CHART_HPADDING, y, chartWidth, chartHeight - CHART_SEPARATOR - 1);
-
-      // Draw the grid lines
-      g.setColor(CHART_LINE);
-      Stroke strokeOrig = ((Graphics2D) g).getStroke();
-      for (int x = CHART_HPADDING; x < chartWidth; x += DXINCR) {
-        Stroke stroke = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {1}, 0);
-        ((Graphics2D) g).setStroke(stroke);
-        g.drawLine(x, y, x, y + chartHeight - CHART_SEPARATOR - 1);
-      }
       
       // Draw the rectangle
       ((Graphics2D) g).setStroke(strokeOrig);
@@ -100,17 +93,22 @@ public class ChartsPanel extends JPanel {
       // Draw the label
       g.setColor(CHART_FONT);
       g.drawString(chart.getLabel(), CHART_HPADDING, y - CHART_VPADDING);
-
+      
+      // Draw the grid lines
+      g.setColor(CHART_LINE);
+      ((Graphics2D) g).setStroke(strokeGridlines);
+      for (int x = CHART_HPADDING + DXINCR; x < chartWidth; x += DXINCR) {
+        g.drawLine(x, y, x, y + chartHeight - CHART_SEPARATOR - 1);
+      }
+      
       y += chartHeight;
     }
     
     g.setColor(CHART_FONT);
-    for (int idx = 0, x = CHART_HPADDING; idx < timeSeries.getTimeSeriesData().size() && x < chartWidth; idx++, x += DXINCR) {
-      int monthPrev = -1;
-      if (idx > 0) {
-        cal.setTime(timeSeries.getTimeSeriesData().get(idx - 1).getDate());
-        monthPrev = cal.get(Calendar.MONTH);
-      }
+    for (int idx = 1, x = CHART_HPADDING + DXINCR; idx < timeSeries.getTimeSeriesData().size() && x < chartWidth; idx++, x += DXINCR) {
+      cal.setTime(timeSeries.getTimeSeriesData().get(idx - 1).getDate());
+      int monthPrev = cal.get(Calendar.MONTH);
+
       cal.setTime(timeSeries.getTimeSeriesData().get(idx).getDate());
       int month = cal.get(Calendar.MONTH);
       
@@ -118,7 +116,6 @@ public class ChartsPanel extends JPanel {
         g.drawString(Utils.getMonthString(cal), x, y - m.getHeight());
       }
     }
-    
     
     /*
     g.drawLine(0, 0, 100, 100);
