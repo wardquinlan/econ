b0e9fc3a2b9b00f40dc5f5b50fe644ae686dc92b
Ward Quinlan <wardquinlan@outlook.com>
2023-01-27 06:30:13 -0500

    refactor package structure

--- resources/econ.sh
+++ resources/econ.sh
@@ -10,5 +10,5 @@ CLASSPATH=$CLASSPATH:$ECON_HOME/econ.d/econ.jar
 CLASSPATH=$CLASSPATH:$ECON_HOME/econ.d/log4j-1.2.14.jar
 CLASSPATH=$CLASSPATH:$ECON_HOME/econ.d/commons-logging-1.1.jar
 CLASSPATH=$CLASSPATH:$ECON_HOME/econ.d/postgresql-9.3-1104.jdbc41.jar
-CMD="$JAVA_HOME/bin/java -classpath $CLASSPATH -Xmx128m econ.Main $@"
+CMD="$JAVA_HOME/bin/java -classpath $CLASSPATH -Xmx128m econ.core.Main $@"
 eval $CMD
