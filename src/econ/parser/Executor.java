4687856b68cc19acac33db8e782b25aebd181869
Ward Quinlan <wardquinlan@outlook.com>
2023-02-06 21:34:47 -0500

    more unary changes

--- src/econ/parser/Executor.java
+++ src/econ/parser/Executor.java
@@ -15,6 +15,22 @@ public class Executor {
     this.operator = operator;
   }
   
+  public Object exec(Object val1) throws Exception {
+    if (val1 instanceof TimeSeries) {
+      TimeSeries timeSeries1 = (TimeSeries) val1;
+      TimeSeries timeSeries = new TimeSeries(operator.getAssociatedSeriesType());
+      for (TimeSeriesData timeSeriesData1: timeSeries1.getTimeSeriesDataList()) {
+        TimeSeriesData timeSeriesData = new TimeSeriesData();
+        timeSeriesData.setDate(timeSeriesData1.getDate());
+        timeSeriesData.setValue(((UnaryOperator) operator).exec(timeSeriesData1.getValue()));
+        timeSeries.add(timeSeriesData);
+      }
+      return timeSeries;
+    } else {
+      return ((UnaryOperator) operator).exec(val1);
+    }
+  }
+  
   public Object exec(Object val1, Object val2) throws Exception {
     if (val1 instanceof TimeSeries && val2 instanceof Integer) {
       return exec((TimeSeries) val1, (Integer) val2);
