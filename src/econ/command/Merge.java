70afc58f0913206d8ee420a9f8d1461396154cb5
Ward Quinlan <wardquinlan@outlook.com>
2023-02-12 08:01:26 -0500

    continued merge

--- src/econ/command/Merge.java
+++ src/econ/command/Merge.java
@@ -5,13 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import econ.core.TimeSeries;
 import econ.core.Utils;
 import econ.parser.Symbol;
 
 public class Merge implements Command {
   @Override
   public String getSummary() {
-    return "int    merge()";
+    return "int    merge(Series series)";
   }
   
   @Override
@@ -29,6 +30,17 @@ public class Merge implements Command {
   @Override
   public Object run(Map<String, Symbol> symbolTable, File file, List<Object> params) throws Exception {
     Utils.validate(params, 1, 1);
+    if (!(params.get(0) instanceof TimeSeries)) {
+      throw new Exception("'series' is not a Series");
+    }
+    TimeSeries timeSeries = (TimeSeries) params.get(0);
+    if (timeSeries.getId() == null) {
+      throw new Exception("series id not set");
+    }
+    TimeSeries timeSeriesDS = Utils.load(timeSeries.getId());
+    if (timeSeriesDS == null) {
+      throw new Exception("series not found");
+    }
     return 0;
   }
 }
