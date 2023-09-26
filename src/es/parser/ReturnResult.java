3bf5e978aed1fa2d4fd32d5c67540f5cf5cf3f7a
Ward Quinlan <wardquinlan@outlook.com>
2023-09-26 09:49:55 -0400

    more if statement stuff

---
+++ src/es/parser/ReturnResult.java
@@ -0,0 +1,13 @@
+package es.parser;
+
+public class ReturnResult {
+  private Object value;
+  
+  public ReturnResult(Object value) {
+    this.value = value;
+  }
+  
+  public Object getValue() {
+    return value;
+  }
+}
