bec2ccafed6d3e513a25bb5282327e7e67ed2d3b
Ward Quinlan <wardquinlan@outlook.com>
2023-10-12 13:50:19 -0400

    continuation

--- src/es/command/Cat.java
+++ src/es/command/Cat.java
@@ -4,15 +4,12 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import es.core.ESIterator;
 import es.core.TimeSeries;
 import es.core.Utils;
-import es.parser.Function;
-import es.parser.OldParser;
-import es.parser.ReturnResult;
+import es.parser.FunctionCaller;
+import es.parser.FunctionDeclaration;
 import es.parser.Symbol;
 import es.parser.SymbolTable;
-import es.parser.Token;
 
 public class Cat implements Command {
   private static final int TIME_SERIES_COL_WIDTHS[] = {20, 5, 20, 30, 8, 12, 20};
@@ -68,34 +65,18 @@ public class Cat implements Command {
   }
 
   private Object runAsIterator(SymbolTable symbolTable, File file, List<Object> params) throws Exception {
-    if (!(params.get(0) instanceof Function)) {
+    if (!(params.get(0) instanceof FunctionDeclaration)) {
       throw new Exception(params.get(0) + " is not a function");
     }
-    Function function = (Function) params.get(0);
-    if (function.getParams().size() != 1) {
-      throw new Exception("wrong number of params in function call (must be 1)");
-    }
-    if (function.getTokenList().size() > 0) {
-      List<String> list = new ArrayList<>();
-      for(String key: symbolTable.keySet()) {
-        Symbol symbol = symbolTable.get(key);
-        if (symbol.getValue() instanceof TimeSeries) {
-          list.add(key);
-        }
-      }
-      for (String name: list) {
-        /*
-        TimeSeries timeSeries = (TimeSeries) symbolTable.get(name).getValue();
-        SymbolTable childSymbolTable = new SymbolTable(symbolTable);
-        childSymbolTable.localPut(function.getParams().get(0), new Symbol(timeSeries));
-        OldParser parser = new OldParser(childSymbolTable);
-        ESIterator<Token> itr2 = new ESIterator<Token>(function.getTokenList());
-        Token tk2 = itr2.next();
-        ReturnResult returnResult = parser.parse(tk2, itr2);
-        if (returnResult != null && returnResult.getValue() != null) {
-          throw new Exception("cannot return anything during iterations: " + returnResult.getValue());
-        }
-        */
+    FunctionDeclaration functionDeclaration = (FunctionDeclaration) params.get(0);
+    FunctionCaller functionCaller = new FunctionCaller();
+    for(String key: symbolTable.keySet()) {
+      Symbol symbol = symbolTable.get(key);
+      if (symbol.getValue() instanceof TimeSeries) {
+        TimeSeries timeSeries = (TimeSeries) symbol.getValue();
+        List<Object> list = new ArrayList<>();
+        list.add(timeSeries);
+        functionCaller.invokeFunction(functionDeclaration.getName(), symbolTable, file, list);
       }
     }
     return null;
