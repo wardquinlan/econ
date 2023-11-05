94b7ff7ef91712cc38e8b15aec6e16a94c0fb99d
Ward Quinlan <wardquinlan@outlook.com>
2023-11-05 15:28:00 -0500

    loops

--- src/es/parser/LoopStatement.java
+++ src/es/parser/LoopStatement.java
@@ -12,10 +12,40 @@ public class LoopStatement extends Statement {
   public LoopStatement(Object predicate) {
     this.predicate = predicate;
   }
-  
+
   @Override
   public Object evaluate(SymbolTable symbolTable) throws Exception {
+    whileloop:
+    while (evaluatePredicate(symbolTable)) {
+      forloop:
+      for (Statement statement: body) {
+        if (statement instanceof BreakStatement) {
+          break whileloop;
+        } else if (statement instanceof ContinueStatement) {
+          break forloop;
+        } else {
+          Object result2 = statement.evaluate(symbolTable);
+          if (result2 instanceof Return) {
+            return result2;
+          }
+        }
+      }
+    }
     return null;
+  }
+
+  public boolean evaluatePredicate(SymbolTable symbolTable) throws Exception {
+    if (predicate instanceof Boolean) {
+      return (Boolean) predicate;
+    } else if (predicate instanceof Evaluable) {
+      Object tmp = ((Evaluable) predicate).evaluate(symbolTable);
+      if (!(tmp instanceof Boolean)) {
+        throw new Exception("predicate does not evaluate to a boolean expression");
+      }
+      return (Boolean) tmp;
+    } else {
+      throw new Exception("predicate does not evaluate to a boolean expression");
+    }
   }
   
   public List<Statement> getBody() {
