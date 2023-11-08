85b45fbb82e1d938dd5edf8638fc882b1feaadb7
Ward Quinlan <wardquinlan@outlook.com>
2023-11-08 11:21:11 -0500

    remove ESC b.c. it interferes with tool tips

--- src/es/gui/ChartsPanel.java
+++ src/es/gui/ChartsPanel.java
@@ -137,7 +137,6 @@ public class ChartsPanel extends JPanel {
           }
           return;
         case KeyEvent.VK_F5:
-        case KeyEvent.VK_ESCAPE:
           clearDecorations();
           repaint();
           return;
