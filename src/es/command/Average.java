f3e9e01d328dafc8b014c4a29bc1f745d530900a
Ward Quinlan <wardquinlan@outlook.com>
2023-11-11 19:29:21 -0500

    Updates to :Average

--- src/es/command/Average.java
+++ src/es/command/Average.java
@@ -12,13 +12,13 @@ import es.parser.SymbolTable;
 public class Average implements Command {
   @Override
   public String getSummary() {
-    return "Series  " + Utils.ROOT_NAMESPACE + "Average(Series series, int n);";
+    return "Object  " + Utils.ROOT_NAMESPACE + "Average(Series series[, int n]);";
   }
   
   @Override
   public List<String> getDetails() {
     List<String> list = new ArrayList<>();
-    list.add("Calculates the 'n'-period moving average of 'series'");
+    list.add("If 'n' is present, calculates the n-period moving average of 'series'.  Otherwise, calculates the absolute series average.");
     list.add("");
     list.add("Note that 0 < n <= size(series)");
     return list;
@@ -26,12 +26,12 @@ public class Average implements Command {
   
   @Override
   public String getReturns() {
-    return "'n'-period moving average of 'series'";
+    return "n-period moving average of 'series' (if 'n' is present), or the absolute series average";
   }
   
   @Override
   public Object run(SymbolTable symbolTable, File file, List<Object> params) throws Exception {
-    Utils.validate(params, 2, 2);
+    Utils.validate(params, 1, 2);
     
     if (!(params.get(0) instanceof TimeSeries)) {
       throw new Exception(params.get(0) + " is not a Series");
@@ -40,7 +40,9 @@ public class Average implements Command {
     if (timeSeries.getType() != TimeSeries.FLOAT) {
       throw new Exception(params.get(0) + " is not a FLOAT series");
     }
-    
+    if (params.size() == 1) {
+      return average(timeSeries);
+    }
     if (!(params.get(1) instanceof Integer)) {
       throw new Exception(params.get(1) + " is not an int");
     }
@@ -62,5 +64,17 @@ public class Average implements Command {
       timeSeriesAvg.add(timeSeriesData);
     }
     return timeSeriesAvg;
+  }
+  
+  private float average(TimeSeries timeSeries) throws Exception {
+    List<TimeSeriesData> list = timeSeries.getTimeSeriesDataList();
+    if (list.size() == 0) {
+      throw new Exception("series is empty: " + timeSeries);
+    }
+    float sum = 0;
+    for (int i = 0; i < list.size(); i++) {
+      sum += (float) list.get(i).getValue();
+    }
+    return sum / list.size();
   }
 }
