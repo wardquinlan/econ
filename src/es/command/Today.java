ab5f5a15ef84ffefa1bfcab6b304aeb6e0d5bfb5
Ward Quinlan <wardquinlan@outlook.com>
2023-11-12 11:49:40 -0500

    :Today()

--- src/es/command/Today.java
+++ src/es/command/Today.java
@@ -11,24 +11,24 @@ import es.parser.SymbolTable;
 public class Today implements Command {
   @Override
   public String getSummary() {
-    return "String  " + Utils.ROOT_NAMESPACE + "Today();";
+    return "Date    " + Utils.ROOT_NAMESPACE + "Today();";
   }
   
   @Override
   public List<String> getDetails() {
     List<String> list = new ArrayList<>();
-    list.add("Returns a string representation of today's date");
+    list.add("Returns today's date");
     return list;
   }
   
   @Override
   public String getReturns() {
-    return "Today's date as a string";
+    return "Today's date";
   }
   
   @Override
   public Object run(SymbolTable symbolTable, File file, List<Object> params) throws Exception {
     Utils.validate(params, 0, 0);
-    return Utils.DATE_FORMAT.format(new Date());
+    return Utils.DATE_FORMAT.parse(Utils.DATE_FORMAT.format(new Date()));
   }
 }
